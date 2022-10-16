fdb327d0eec4922fd5eb099562c32820930e97b3
fifu09 <fifu1999@gmail.com>
2022-10-16 18:34:33 +0200

    Duplicate codesmell linea 222

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -220,33 +220,38 @@ public class DataAccess  {
 			Registered reg4 = new Registered("mikel", "123", 1111);
 									
 			if (Locale.getDefault().equals(new Locale("es"))) {
-				q1=ev1.addQuestion("¿Quién ganará el partido?",1);
+				String pregunta1 = "¿Quién ganará el partido?";
+				q1=ev1.addQuestion(pregunta1,1);
 				q2=ev1.addQuestion("¿Quién meterá el primer gol?",2);
-				q3=ev11.addQuestion("¿Quién ganará el partido?",1);
+				q3=ev11.addQuestion(pregunta1,1);
 				q4=ev11.addQuestion("¿Cuántos goles se marcarán?",2);
-				q5=ev17.addQuestion("¿Quién ganará el partido?",1);
+				q5=ev17.addQuestion(pregunta1,1);
 				q6=ev17.addQuestion("¿Habrá goles en la primera parte?",2);
 				
 			}
+			
 			else if (Locale.getDefault().equals(new Locale("en"))) {
-				q1=ev1.addQuestion("Who will win the match?",1);
+				String pregunta2 = "Who will win the match?";
+				q1=ev1.addQuestion(pregunta2,1);
 				q2=ev1.addQuestion("Who will score first?",2);
-				q3=ev11.addQuestion("Who will win the match?",1);
+				q3=ev11.addQuestion(pregunta2,1);
 				q4=ev11.addQuestion("How many goals will be scored in the match?",2);
-				q5=ev17.addQuestion("Who will win the match?",1);
+				q5=ev17.addQuestion(pregunta2,1);
 				q6=ev17.addQuestion("Will there be goals in the first half?",2);
 				
 			}			
 			else {
-				q1=ev1.addQuestion("Zeinek irabaziko du partidua?",1);
+				String pregunta3 = "Zeinek irabaziko du partidua?";
+				q1=ev1.addQuestion(pregunta3,1);
 				q2=ev1.addQuestion("Zeinek sartuko du lehenengo gola?",2);
-				q3=ev11.addQuestion("Zeinek irabaziko du partidua?",1);
+				q3=ev11.addQuestion(pregunta3,1);
 				q4=ev11.addQuestion("Zenbat gol sartuko dira?",2);
-				q5=ev17.addQuestion("Zeinek irabaziko du partidua?",1);
+				q5=ev17.addQuestion(pregunta3,1);
 				q6=ev17.addQuestion("Golak sartuko dira lehenengo zatian?",2);
 				
 				
 			}
+			
 			q7 = ev21.addQuestion("Emaitza?", 1);
 			q8 = ev21.addQuestion("Emaitza?", 1);
 			q9 = ev22.addQuestion("Irabazlea?", 1.5);
