2f7b0d031055a1af15f38be04b5c2d584d595bc2
Abderraouf Khedidji <70901925+fifu09@users.noreply.github.com>
2022-10-25 10:26:07 +0200

    Update gertaeraEzabatuDAB.java
    gertaEraEzabatuDAB mejorado

--- src/test/java/gertaeraEzabatu/gertaeraEzabatuDAB.java
+++ src/test/java/gertaeraEzabatu/gertaeraEzabatuDAB.java
@@ -1,107 +1,147 @@
-package gertaeraEzabatu;
+package emaitzakIpini;
+
 import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import configuration.UtilDate;
 import dataAccess.DataAccess;
+import domain.ApustuAnitza;
+import domain.Apustua;
 import domain.Event;
+import domain.Question;
+import domain.Quote;
+import domain.Registered;
+import domain.Sport;
 import domain.Team;
+import domain.Transaction;
 import test.dataAccess.TestDataAccess;
 
-public class gertaeraEzabatuDAB {
-
-	static DataAccess dataAccess = new DataAccess();
-	static TestDataAccess testDA = new TestDataAccess();
-	Date eventDate;
-
+public class emaitzakIpiniDAB {
+	
+	Date eventDate = null;
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+	
+	
+	static TestDataAccess testDA = new TestDataAccess();
+	static DataAccess dataAccess = new DataAccess();
+	
 	public void cambiarFecha(String fecha) {
-		try {
-			eventDate = sdf.parse(fecha);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+        try {
+            eventDate = sdf.parse(fecha);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	Quote quote1 = null;
+	
+	@Before
+	public void init() {
+		Calendar today = Calendar.getInstance();
+		int month=today.get(Calendar.MONTH);
+		month+=1;
+		int year=today.get(Calendar.YEAR);
+		if (month==12) { month=0; year+=1;}
+		Team team1= new Team("Atletico");
+		Team team2= new Team("Athletic");
+		Event ev1=new Event(1, "Atletico-Athletic", UtilDate.newDate(year,month,17), team1, team2);
+		Sport sp1=new Sport("Futbol");
+		sp1.addEvent(ev1);
+		ev1.setSport(sp1);
+		Question q1;
+		String pregunta1 = "Â¿QuiÃ©n ganarÃ¡ el partido?";
+		q1=ev1.addQuestion(pregunta1,1);
+		quote1 = q1.addQuote(1.3, "1", q1);
+		Registered reg1 =new Registered("registered", "123", 1234);
+		ApustuAnitza apA1 = new ApustuAnitza(reg1, 5.0);
+		Apustua ap1 = new Apustua(apA1, quote1);
+		apA1.addApustua(ap1);
+		quote1.addApustua(ap1);
+		ap1.eguneratuApustuKant(sp1);
+		String a = "ApustuaEgin";
+		Transaction t1 = new Transaction(reg1, apA1.getBalioa(), new Date(), a);
+		reg1.addTransaction(t1);
+		team1.addEvent(ev1);
+		
 	}
-	// Evento NULL
+	
 	@Test
+	//Probando con q==null
 	public void test1() {
 		try {
-			boolean result = dataAccess.gertaeraEzabatu(null);
-			assertFalse(result);
-			System.out.println("SUCCESS");
-		} catch (Exception e) {
-			System.out.println("FAIL");
+			testDA.EmaitzakIpini(null);
+			fail("El metodo ha funcionado cuando no deberia");
+		}catch(Exception e) {
+			System.out.println("El test 1 ha funcionado como deberia");
 		}
 	}
-
-	// Evento que aun no ha ocurrido
 	
 	@Test
+	//Probando con q que no este en la base de datos
 	public void test2() {
-		cambiarFecha("21/07/2023");
-		String description = "Team1-Team2";
-		String[] teams = description.split("-");
-		Team team1 = new Team(teams[0]);
-		Team team2 = new Team(teams[1]);
-		Event ev1 = null;
+		
 		try {
-			// Anadimos el evento con la pregunta
-			testDA.open();
-			ev1 = testDA.addEventWithQuestion(1, description, eventDate, "pregunta de prueba", 0, team1, team2);
-			testDA.close();
 			
-			// Intentamos eliminar el evento 
-			boolean result = dataAccess.gertaeraEzabatu(ev1);
-			
-			// El metodo rechaza eliminar un evento que aun no ha ocurrido
-			assertFalse(result);
-			System.out.println("SUCCESS");
-		} catch (Exception e) {
-			fail("FAIL" + e.getMessage());
-		}finally {
 			testDA.open();
-			testDA.removeEvent(ev1);
+			testDA.EmaitzakIpini(quote1);
 			testDA.close();
+			fail("El test 2 ha fallado");
+			
+		
+			
+		}catch(Exception e) {
+			System.out.println("El test 2 ha funcionado como deberia");
+		}finally{
+			testDA.Clear();
 		}
 	}
-
-	// Evento que ya ha ocurrido y tiene p�blicos todos los resultados
 	
 	@Test
+	//Probando con q que tenga el evento con una fecha que aun no ha pasado
 	public void test3() {
-			cambiarFecha("21/07/2022");
-			String description = "Team1-Team2";
-			String[] teams = description.split("-");
-			Team team1 = new Team(teams[0]);
-			Team team2 = new Team(teams[1]);
-			Event ev1 = null;
-			try {
-				
-				testDA.open();
-				ev1 = testDA.addEventWithQuestion(1, description, eventDate, "pregunta de prueba", 0, team1, team2);
-				testDA.close();
-				
-				boolean result = dataAccess.gertaeraEzabatu(ev1);
-				
-				// Comprobamos si existe dicho metodo
-				testDA.open();
-				boolean notExpect = !testDA.existEvent(ev1);
-				testDA.close();
-				
-				// El result deberia ser true (buena eliminacion) y el notExpect deberia ser false porque no encuentra el metodo
-				assertEquals(result, notExpect);
-				System.out.println("SUCCESS");
-				
-		} catch (Exception e) {
-			fail("FAIL" + e.getMessage());
+		
+		try {
+			testDA.open();
+			testDA.EmaitzakIpini(quote1);
+			testDA.close();
+			fail("El test 3 ha fallado");
+			
+		}catch(Exception e) {
+			System.out.println("El test 3 ha funcionado como deberia");
+		}finally{
+			testDA.Clear();
+		}
+	}
+	@Test
+	//Probando con q que tenga el evento con una fecha que aun no ha pasado
+	public void test4() {
+		
+		try {
+			dataAccess.open(false);
+			dataAccess.EmaitzakIpini(quote1);
+			dataAccess.close();
+			
+			System.out.println("El test 4 ha funcionado como deberia");
+		
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			fail("El test 4 ha fallado" + e.getMessage());
+			
 		}finally {
 			testDA.open();
-			testDA.removeEvent(ev1);
+			// testDA.MetodoQueDeshaceElEmaitzakIpini();
 			testDA.close();
 		}
 	}
 }
+
