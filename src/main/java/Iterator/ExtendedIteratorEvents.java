47d0f46ffc07a45236703c2f2d35692d09fe6e26
nmania3 <niegggg@gmail.com>
2022-11-10 18:35:45 +0100

    Empezando con el iterator

---
+++ src/main/java/Iterator/ExtendedIteratorEvents.java
@@ -0,0 +1,47 @@
+package Iterator;
+
+import java.util.List;
+
+import domain.Event;
+
+public class ExtendedIteratorEvents implements ExtendedIterator {
+	List<Event> eventos;
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	@Override
+	public Event next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Event previous() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void goFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void goLast() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
